dc9f3d838d9ac866b9932d58d3c2a4297ad92af8
onoie <onoie3@gmail.com>
2017-09-08 11:45:26 +0900

    add sqlite sample script

--- op.py
+++ op.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 print('hello,op!')
+
+# import sqlite3
+#
+# dbname = 'op.db'
+#
+# conn = sqlite3.connect(dbname)
+# c = conn.cursor()
+#
+# # executeメソッドでSQL文を実行する
+# create_table = '''create table users (id int, name varchar(64),
+#                   age int, gender varchar(32))'''
+# c.execute(create_table)
+#
+# # SQL文に値をセットする場合は，Pythonのformatメソッドなどは使わずに，
+# # セットしたい場所に?を記述し，executeメソッドの第2引数に?に当てはめる値を
+# # タプルで渡す．
+# sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
+# user = (1, 'Taro', 20, 'male')
+# c.execute(sql, user)
+#
+# # 一度に複数のSQL文を実行したいときは，タプルのリストを作成した上で
+# # executemanyメソッドを実行する
+# insert_sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
+# users = [
+#     (2, 'Shota', 54, 'male'),
+#     (3, 'Nana', 40, 'female'),
+#     (4, 'Tooru', 78, 'male'),
+#     (5, 'Saki', 31, 'female')
+# ]
+# c.executemany(insert_sql, users)
+# conn.commit()
+#
+# select_sql = 'select * from users'
+# for row in c.execute(select_sql):
+#     print(row)
+#
+# conn.close()
