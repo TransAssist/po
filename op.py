70f0fd0e71062a0e558e7426b127a3f994f4c913
onoie <onoie3@gmail.com>
2017-09-08 13:36:25 +0900

    add default cmd & create sqlite cmd

--- op.py
+++ op.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-print('hello,op!')
+# print('hello,op!')
+# quit()
+import sys
+import sqlite3
 
-# import sqlite3
-#
-# dbname = 'op.db'
-#
-# conn = sqlite3.connect(dbname)
-# c = conn.cursor()
-#
-# # executeメソッドでSQL文を実行する
-# create_table = '''create table users (id int, name varchar(64),
-#                   age int, gender varchar(32))'''
-# c.execute(create_table)
-#
-# # SQL文に値をセットする場合は，Pythonのformatメソッドなどは使わずに，
-# # セットしたい場所に?を記述し，executeメソッドの第2引数に?に当てはめる値を
-# # タプルで渡す．
-# sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
-# user = (1, 'Taro', 20, 'male')
-# c.execute(sql, user)
-#
-# # 一度に複数のSQL文を実行したいときは，タプルのリストを作成した上で
-# # executemanyメソッドを実行する
-# insert_sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
-# users = [
-#     (2, 'Shota', 54, 'male'),
-#     (3, 'Nana', 40, 'female'),
-#     (4, 'Tooru', 78, 'male'),
-#     (5, 'Saki', 31, 'female')
-# ]
-# c.executemany(insert_sql, users)
-# conn.commit()
-#
-# select_sql = 'select * from users'
-# for row in c.execute(select_sql):
-#     print(row)
-#
-# conn.close()
+
+def setup_sqlite():
+    print("sqlite function")
+    dbname = 'op.db'
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    # executeメソッドでSQL文を実行する
+    create_table = '''create table users (id int, name varchar(64),age int, gender varchar(32))'''
+    c.execute(create_table)
+    # SQL文に値をセットする場合は，Pythonのformatメソッドなどは使わずに，
+    # セットしたい場所に?を記述し，executeメソッドの第2引数に?に当てはめる値を
+    # タプルで渡す．
+    sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
+    user = (1, 'Taro', 20, 'male')
+    c.execute(sql, user)
+    # 一度に複数のSQL文を実行したいときは，タプルのリストを作成した上で
+    # executemanyメソッドを実行する
+    insert_sql = 'insert into users (id, name, age, gender) values (?,?,?,?)'
+    users = [
+        (2, 'Shota', 54, 'male'),
+        (3, 'Nana', 40, 'female'),
+        (4, 'Tooru', 78, 'male'),
+        (5, 'Saki', 31, 'female')
+    ]
+    c.executemany(insert_sql, users)
+    conn.commit()
+    select_sql = 'select * from users'
+    for row in c.execute(select_sql):
+        print(row)
+    conn.close()
+
+
+def main(cmd='default'):
+    print('opcmd:%s' % (cmd))
+    if cmd != "default":
+        if cmd == "sqlite":
+            setup_sqlite()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) == 1:
+        print("default")
+        main()
+    elif len(args) == 2:
+        main(args[1])
+    else:
+        print("multi args no support")
+        quit()
+
+
+
+
---
+++ operation.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+OPCMD=$(zenity --entry --text="input:")
+./op.py $OPCMD
+
+
